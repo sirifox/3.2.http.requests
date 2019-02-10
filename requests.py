1203f09d869c672d226a7e528757c8f96ec3a0dd
sirifox <45976344+sirifox@users.noreply.github.com>
2019-02-10 20:43:18 +0300

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,1 +1,32 @@
+import requests
 
+
+def translate(way_text, way_result, lang_first, lang_sec='ru'):
+
+    URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    lang = lang_first + '-' + lang_sec
+    params = {'lang': lang, 'srv': 'tr-text', 'id': 'f5c948f1.5c59b9e9.16d39b90-6-0',
+              'key': 'trnsl.1.1.20190210T165957Z.213a8d8cc9ee59bf.316e87145b5e5bf90130e772fba39bc568290b4a'}
+    write_list = []
+    with open(way_text, encoding='utf-8') as f:
+        for line in f.readlines():
+            data = {'text': line}
+            resp = requests.get(URL, data=data, params=params)
+            write_list.append(resp.json()['text'][0])
+
+    write_str = ''.join(write_list)
+    with open(way_result, 'w', encoding='utf-8') as f_r:
+        f_r.write(write_str)
+
+    return 'Перевод осуществлён'
+
+
+print(translate('3.2.http.requests\\DE.txt',
+                '3.2.http.requests\\DE_RU.txt',
+                'de'))
+print(translate('3.2.http.requests\\FR.txt',
+                '3.2.http.requests\\FR_RU.txt',
+                'fr'))
+print(translate('3.2.http.requests\\ES.txt',
+                '3.2.http.requests\\ES_RU.txt',
+                'es'))
